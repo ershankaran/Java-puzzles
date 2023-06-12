153d9fadcafff80f88366a6ee68510b2f77e492e
shankaran314 <shankaran.er@outlook.com>
2023-06-12 20:28:07 +0530

    base commit

---
+++ java-prereq-OOPS/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+          /* 
+       1.Method is simply a collection of instructions and an absolute build block of java
+       2. Class is the container of fields/attributes and methods.
+       3. main method is the starting point of the java
+       */ 
+    }
+}
