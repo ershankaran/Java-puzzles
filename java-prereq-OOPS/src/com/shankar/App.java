895e6321dc38eb0cdba89e012fc038c95e274374
shankaran314 <shankaran.er@outlook.com>
2023-06-12 20:42:36 +0530

    variables

--- java-prereq-OOPS/src/com/shankar/App.java
+++ java-prereq-OOPS/src/com/shankar/App.java
@@ -1,12 +1,25 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
           /* 
        1.Method is simply a collection of instructions and an absolute build block of java
        2. Class is the container of fields/attributes and methods.
        3. main method is the starting point of the java
        */ 
 
+       System.out.println("Hello, World!");
+
+       String myVar = "Good day 123";
+
+       System.out.println(myVar);
+
+       myVar = "";
+       
+       System.out.println(myVar);
+
+
+       printHello();
        printHello();
     }
 
